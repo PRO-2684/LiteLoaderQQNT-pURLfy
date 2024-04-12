5d307c08965bfa0ff1123ba89dc03922b898cc8a
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2024-04-12 08:59:32 +0800

    Sync pURLfy v0.1.7

--- purlfy.js
+++ purlfy.js
@@ -230,39 +230,37 @@ class Purlfy extends EventTarget {
             urlObj = new URL(originalUrl);
         } else {
             log(`Cannot parse URL ${originalUrl}`);
-            return originalUrl;
+            return {
+                url: originalUrl,
+                rule: "N/A"
+            }
         }
         while (shallContinue && iteration++ < this.maxIterations) {
             const logi = (...args) => this.#log(`[#${iteration}]`, ...args);
             const protocol = urlObj.protocol;
             if (protocol !== "http:" && protocol !== "https:") { // Not a valid HTTP URL
                 logi(`Not a HTTP URL: ${urlObj.href}`);
-                return urlObj.href;
+                break;
             }
             const hostAndPath = urlObj.host + urlObj.pathname;
             const parts = hostAndPath.split("/").filter(part => part !== "");
             const rule = this.#matchRule(parts);
             if (!rule) { // No matching rule found
                 logi(`No matching rule found for ${urlObj.href}.`);
-                return urlObj.href;
+                break;
             }
             firstRule ??= rule;
             logi(`Matching rule: ${rule.description} by ${rule.author}`);
             [urlObj, shallContinue] = await this.#applyRule(urlObj, rule, logi);
             logi("Purified URL:", urlObj.href);
         }
-        if (originalUrl === urlObj.href) { // No changes made
-            this.#log("No changes made.");
-            return {
-                url: originalUrl,
-                rule: `* ${firstRule.description} by ${firstRule.author}`
-            };
+        if (firstRule) { // Increment statistics only if a rule was applied
+            this.#statistics.url++;
+            this.#onStatisticsChange();
         }
-        this.#statistics.url++;
-        this.#onStatisticsChange();
         return {
             url: urlObj.href,
-            rule: `${firstRule.description} by ${firstRule.author}`
+            rule: firstRule ? `${firstRule.description} by ${firstRule.author}` : "N/A"
         };
     }
 }
